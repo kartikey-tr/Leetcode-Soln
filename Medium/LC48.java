dc585c7a60e713e70a01f430f3f7bcedaa907fc4
Kartikey Tripathi <kartikey_tripathi@outlook.com>
2026-03-23 19:51:32 +0530

    LeetCode 48

---
+++ Medium/LC48.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                int temp = matrix [i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int si = 0, ei = n - 1;
+
+            while (si < ei) {
+                int temp = matrix[i][si];
+                matrix[i][si] = matrix[i][ei];
+                matrix[i][ei] = temp;
+
+                si++;
+                ei--;
+            }
+        }
+    }
+}
